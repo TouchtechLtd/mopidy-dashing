b5f7e1ef93243855cd3125f761df610c6deac36b
Zaffron Saffron <zaffron@touchtech.co.nz>
2015-07-10 17:44:15 +1200

    Add support for custom music widget

--- mopidy_dashing/frontend.py
+++ mopidy_dashing/frontend.py
@@ -6,62 +6,72 @@ import logging
 import json
 
 from mopidy import core
+from time import sleep
 
 logger = logging.getLogger(__name__)
 
 class DashingFrontend(pykka.ThreadingActor, core.CoreListener):
-	def __init__(self, config, core):
-		super(DashingFrontend, self).__init__()
-		self.config = config	
-		self.core = core
+    def __init__(self, config, core):
+        super(DashingFrontend, self).__init__()
+        self.config = config    
+        self.core = core
 
-		self.url = "http://%s:%s%s" % (
-			config['dashing']['hostname'], 
-			config['dashing']['port'], 
-			config['dashing']['widget'],
-		)
-		self.auth_token = config['dashing']['auth_token']
+        self.url = "http://%s:%s%s" % (
+            config['dashing']['hostname'], 
+            config['dashing']['port'], 
+            config['dashing']['widget'],
+        )
+        self.auth_token = config['dashing']['auth_token']
 
-		self.req = urllib2.Request(self.url)
-		self.req.add_header('Content-Type', 'application/json')
+        self.req = urllib2.Request(self.url)
+        self.req.add_header('Content-Type', 'application/json')
+    
+    def send(self,data):
+        self.req = urllib2.Request(self.url)
+        self.req.add_header('Content-Type', 'application/json')
+        urllib2.urlopen(self.req, data)
 
-	def on_start(self):
-		logger.debug(self.url)
+    def on_start(self):
+        logger.debug(self.url)
 
-	def on_stop(self):
-		message = json.dumps({
-			"auth_token": self.auth_token,
-			"title": "Stopped"
-		})
+    def on_stop(self):
+        message = json.dumps({
+            "auth_token": self.auth_token,
+            "title": "Music stopped",
+            "song": "",
+            "details": ""
+        })
 
-		logger.info(message)
+        logger.info(message)
+        self.send(message)
 
-#		urllib2.urlopen(self.req, message)
+    def track_playback_started(self, tl_track):
+        self.starting = True
+        artists = []
+        for artist in tl_track.track.artists:
+            artists.append(artist.name)         
+        
+        message = json.dumps({ 
+            "auth_token": self.auth_token,
+            "title" : "Currently playing",
+            "song": tl_track.track.name,
+            "details": "%s - %s" % ("/".join(artists), tl_track.track.album.name)
+        })
 
-	def track_playback_started(self, tl_track):
-		artists = []
+        logger.info(message)
+        self.send(message)
 
-		for artist in tl_track.track.artists:
-			artists.append(artist.name)			
-		
-		message = json.dumps({ 
-			"auth_token": self.auth_token, 
-			"title": tl_track.track.name,
-			"text": "%s - %s" % ("/".join(artists), tl_track.track.album.name),
-		})
-
-		logger.info(message)
-
-		urllib2.urlopen(self.req, message)
-
-	def track_playback_ended(self, tl_track, time_position):
-
-		message = json.dumps({
-			"auth_token": self.auth_token,
-			"title": "Stopped"
-		})
-
-		logger.info(message)
-
-		urllib2.urlopen(self.req, message)
-		
+    def track_playback_ended(self, tl_track, time_position):
+        sleep(1)
+        if not self.starting:
+            message = json.dumps({
+                "auth_token": self.auth_token,
+                "title": "Music stopped",
+                "song": "",
+                "details": ""
+            })
+            logger.info(message)
+            self.send(message)
+        else:
+            self.starting = False
+        
